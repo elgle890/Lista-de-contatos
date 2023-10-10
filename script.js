004023040c47a0e8be8b4c0c176e85b86a4524b6
Arthur <arthurpecegueiro2@gmail.com>
2023-10-10 23:59:16 +1000

    files added

---
+++ script.js
@@ -0,0 +1,44 @@
+const button = document.querySelector('form');
+let linhas = '';
+let input_nome;
+let input_tel;
+let total = 0;
+let numContatos = document.querySelector('p');
+let nomes = [];
+let numeros = [];
+
+button.addEventListener('submit', function(e) {
+    e.preventDefault();
+    adicionaContato();
+    colocaContato();
+});
+
+function adicionaContato() {
+    input_nome = document.getElementById('nome');
+    input_tel = document.getElementById('telefone');
+
+    if (input_nome.value === '' || input_tel.value === '') {
+        alert('Complete todos os dados!');
+    } else {
+
+        if (numeros.includes(input_tel.value)) {
+            alert('Número de telefone já existe!');
+        } else {
+            let linha = `<tr><td>${input_nome.value}</td><td>${input_tel.value}</td></tr>`;
+            linhas += linha;
+            nomes.push(input_nome.value);
+            numeros.push(input_tel.value);
+        }
+
+    }
+
+    input_nome.value = '';
+    input_tel.value = '';
+}
+
+function colocaContato() {
+    let corpo = document.querySelector('tbody');
+    corpo.innerHTML = linhas;
+    total += 1;
+    numContatos.innerHTML = `Total de contatos: ${total}`;
+}
